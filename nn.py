24411d22bc8498bbf7be5782c6f6dee060cdb0b1
Jonathan Few <33668959+ackhia@users.noreply.github.com>
2020-10-06 16:26:36 +0700

    Feedforward (#1)
    * Feedforward without activation
    
    * Simplified feedforward
    
    * Relu activation function

--- nn.py
+++ nn.py
@@ -7,10 +7,22 @@ class Network():
     for i, v in enumerate(shape):
       layer = {}
       layer['activations'] = np.empty(v)
-      layer['biases'] = np.random.rand(v)
       if i != 0:
-        layer['weights'] = np.random.rand(v, shape[i-1])
+        layer['biases'] = np.full(v,0)
+        layer['weights'] = np.array([[0.1,0.2,0.3,0.4],[0.5,0.6,0.7,0.8]])#np.full((v, shape[i-1]), 0.5)
       self.layers.append(layer)
 
+  def predict(self, input):
+    self.layers[0]['activations'] = input
+    for i, layer in enumerate(self.layers[1:], 1):
+      activations = np.dot(layer['weights'], self.layers[i-1]['activations'])
+      activations_bias = np.add(activations, layer['biases'])
+      activations_bias_relu = activations_bias.clip(0, None)
+      layer['activations'] = activations_bias_relu
+    
+    return self.layers[-1]['activations']
+
 if __name__ == "__main__":
-  n = Network([50, 20, 20, 5])
+  n = Network([4, 2])
+  input = np.array([0.1,0.2,0.3,0.4])
+  print(n.predict(input))
