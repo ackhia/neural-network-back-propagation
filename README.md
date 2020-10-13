aa1ba5920b31cc63917a4af4e732dae544f1381a
Jonathan Few <jffreelancing@gmail.com>
2020-10-13 18:39:56 +0700

    Training

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
-# A Neural Network with Back-Propagation
+# A Python Neural Network with Back-Propagation From Scratch
 
 WIP
+
+Features:
+
+* Customisable number of layers and neurons for deep learning
+* Optimised implementation utilising vector operations
+* Written form scratch using only Numpy and Python
+* Modern rectified linear units (ReLU) activation function
+
 
 Data provided by [MNIST](http://yann.lecun.com/exdb/mnist/)
--- nn.py
+++ nn.py
@@ -3,65 +3,101 @@ import numpy as np
 import pandas
 from datareaders import handwriting
 
+
 class Network():
-  def __init__(self, shape):
-    self.shape = shape
-    self.layers = []
-    for i, v in enumerate(shape):
-      layer = {}
-      layer['activations'] = np.empty(v)
-      if i != 0:
-        layer['biases'] = np.full(v, 0.1)
-        layer['weights'] = np.random.rand(v, shape[i-1]) - 0.5
-      self.layers.append(layer)
+    def __init__(self, shape, learning_rate=0.1):
+        self.shape = shape
+        self.layers = []
+        self.learning_rate = learning_rate
 
-  def predict(self, input):
-    self.layers[0]['activations'] = input
-    for i, layer in enumerate(self.layers[1:], 1):
-      activations = np.dot(layer['weights'], self.layers[i-1]['activations'])
-      activations_bias = np.add(activations, layer['biases'])
+        for i, v in enumerate(shape):
+            layer = {}
+            layer['activations'] = np.empty(v)
+            if i != 0:
+                layer['biases'] = np.full(v, 0.1)
+                layer['weights'] = np.random.rand(v, shape[i-1]) - 0.5
+            self.layers.append(layer)
 
-      #Apply relu
-      activations_bias[activations_bias<0] = 0.0
-      
-      #Squash the last layer
-      if i == len(self.layers)-1:
-        activations_bias = activations_bias / np.amax(activations_bias)
-        pass
+    def predict(self, input):
+        self.layers[0]['activations'] = input
+        for i, layer in enumerate(self.layers[1:], 1):
+            activations = np.dot(
+                layer['weights'], self.layers[i-1]['activations'])
+            activations_bias = np.add(activations, layer['biases'])
 
-      layer['activations'] = activations_bias
-    
-    return self.layers[-1]['activations']
+            # Apply relu
+            activations_bias[activations_bias < 0] = 0.0
 
-  def cost(self, dataset):
-    sum = 0.0
-    for image in dataset:
-      actual = self.predict(image['data'])
-      expected = np.zeros(self.shape[-1])
-      expected[image['label']] = 1.0
-      for i, v in enumerate(actual):
-        sum += np.square(v - expected[i])
-    return sum / len(dataset)
+            # Squash the last layer
+            """if i == len(self.layers)-1:
+                max_activation = np.amax(activations_bias)
+                if max_activation > 0:
+                    activations_bias = activations_bias / \
+                        np.amax(activations_bias)"""
 
-  def relu_derivative(self, n):
-    return 1.0 if n > 0 else 0.0
+            layer['activations'] = activations_bias
 
-  def backward_propergate_error(self, expected):
-    for i, layer in zip(range(len(self.layers)-1, 0, -1), self.layers[:0:-1]):
-      if i == len(self.layers) - 1:
-        errors = expected - layer['activations']
-      else:
-        errors = []
-        for j in range(len(layer['weights'])):
-          next_layer_weights = self.layers[i+1]['weights'][:,j]
-          errors.append(np.sum(next_layer_weights * self.layers[i+1]['deltas']))
-      
-      layer['deltas'] = [e * self.relu_derivative(a) for e, a in zip(errors, layer['activations'])]
-        
+        return self.layers[-1]['activations']
+
+    def cost(self, dataset):
+        sum = 0.0
+        for image in dataset:
+            actual = self.predict(image['data'])
+            expected = np.zeros(self.shape[-1])
+            expected[image['label']] = 1.0
+            for i, v in enumerate(actual):
+                sum += np.square(v - expected[i])
+        return sum / len(dataset)
+
+    def relu_derivative(self, n):
+        return 1.0 if n > 0 else 0.0
+
+    def backward_propergate_error(self, expected):
+        for i, layer in zip(range(len(self.layers)-1, 0, -1), self.layers[:0:-1]):
+            if i == len(self.layers) - 1:
+                errors = expected - layer['activations']
+            else:
+                errors = []
+                for j in range(len(layer['weights'])):
+                    next_layer_weights = self.layers[i+1]['weights'][:, j]
+                    errors.append(np.sum(next_layer_weights *
+                                         self.layers[i+1]['deltas']))
+
+            layer['deltas'] = [
+                e * self.relu_derivative(a) for e, a in zip(errors, layer['activations'])]
+
+    def update_weights_biases(self):
+        for i, layer in enumerate(self.layers[1:], 1):
+            for neuron in range(len(layer['weights'])):
+                layer['weights'][neuron] = layer['weights'][neuron] + self.learning_rate * \
+                    layer['deltas'][neuron] * self.layers[i-1]['activations']
+
+    def train_network(self, dataset, epoch_count):
+        for epoch in range(epoch_count):
+            sum_error = 0.0
+
+            for row in dataset:
+                outputs = self.predict(row[:-1])
+                expected = np.zeros(self.shape[-1])
+                expected[int(row[-1])] = 1.0
+                for i, v in enumerate(outputs):
+                    sum_error += np.square(v - expected[i])
+                self.backward_propergate_error(expected)
+                self.update_weights_biases()
+            print(f'Epoch: {epoch}, error_rate {sum_error}')
+
+
+dataset = np.array([[2.7810836, 2.550537003, 0],
+           [1.465489372, 2.362125076, 0],
+           [3.396561688, 4.400293529, 0],
+           [1.38807019, 1.850220317, 0],
+           [3.06407232, 3.005305973, 0],
+           [7.627531214, 2.759262235, 1],
+           [5.332441248, 2.088626775, 1],
+           [6.922596716, 1.77106367, 1],
+           [8.675418651, -0.242068655, 1],
+           [7.673756466, 3.508563011, 1]])
+
 if __name__ == "__main__":
-  n = Network([28*28, 40, 40, 10])
-  dataset = handwriting.get_data_set('test')
-  print('Data set read..')
-  n.predict(dataset[0]['data'])
-  n.backward_propergate_error(dataset[0]['label'])
-  print(n.cost(dataset))
+    n = Network([2, 2, 2])
+    n.train_network(dataset, 200)
